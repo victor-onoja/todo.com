332d047b510f71248bd97dcf8eb6565676376e10
victor-onoja <odohvictor47@gmail.com>
2023-01-17 08:19:45 +0100

    eureka!

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 const todo = document.querySelector('.todoItems');
 const add = document.querySelector('.add');
 const val = document.getElementById('main');
+const del = document.querySelector('.delete');
 
-let items = []; 
+if (localStorage.getItem('myItems')) {
+    items = JSON.parse(localStorage.getItem('myItems'));
+} else {
+    items = [];
+}
 
 add.addEventListener('click', function(){
-items.push(val.value)
-    const listItem = document.createElement('li');
-    items.forEach(function(i){
-        listItem.textContent = i;
-    })
-    todo.appendChild(listItem);  
+   items.push(val.value);
+   localStorage.setItem('myItems', JSON.stringify(items));
+   console.log(items);  
+});
+
+for (let i = 0; i < items.length; i++) {
+    let listItem = document.createElement('li');
+    listItem.innerHTML = items[i] + '.';
+    todo.appendChild(listItem);
+}
+
+del.addEventListener('click', function() {
+    items = [];
+    localStorage.clear();
     console.log(items);
 });
